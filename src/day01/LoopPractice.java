10d0ddb3b21f9d3176854ed0c45985b79beb8322
hasanboyaci15 <hsnbyc@gmail.com>
2020-02-29 14:52:04 -0600

    update

--- src/day01/LoopPractice.java
+++ src/day01/LoopPractice.java
@@ -5,6 +5,9 @@ public class LoopPractice {
         for (int i = 0; i <10 ; i+=2) {
             System.out.println("Counter" +i);
             System.out.println("i = " + i);
+            System.out.println("#########");
+
+
 
             }
     }

c36f8a1793a80585c036ddbf92d0236c250d1122
hasanboyaci15 <hsnbyc@gmail.com>
2020-02-29 14:25:07 -0600

    modified StringPractice class

--- src/day01/StringPractice.java
+++ src/day01/StringPractice.java
@@ -2,6 +2,8 @@ package day01;
 
 public class StringPractice {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world! ");
+        String str="Java is fun";
+        System.out.println("Hello github");
     }
 }

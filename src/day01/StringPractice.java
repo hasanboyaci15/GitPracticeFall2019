ec6e3cbebfaa3cf72e699ad27ef7d9facf1f00c6
hasanboyaci15 <hsnbyc@gmail.com>
2020-02-29 14:27:28 -0600

    another change

--- src/day01/StringPractice.java
+++ src/day01/StringPractice.java
@@ -4,6 +4,6 @@ public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world! ");
         String str="Java is fun";
-        System.out.println("Hello github");
+        System.out.println("Hello github"+ str);
     }
 }
